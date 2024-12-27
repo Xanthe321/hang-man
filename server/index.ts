304ca9655300d543161096ce25c8e360509033a1
Xanthe <104931289+Xanthe321@users.noreply.github.com>
2024-12-27 20:28:23 +0300

    #

--- server/index.ts
+++ server/index.ts
@@ -167,4 +167,8 @@ io.on('connection', (socket) => {
   })
 })
 
-httpServer.listen(3002)
+const PORT = process.env.PORT || 3002
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
